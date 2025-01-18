b452e56b11c02e5c35be9daee772876853892fe6
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2025-01-18 13:36:41 +0530

    push

---
+++ kind-k8s-cluster/setup.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER && newgrp docker
+
+sudo chmod +x install_kind.sh
+./install_kind.sh
+
+sudo chmod +x install_kubectl.sh
+./install_kubectl.sh
+
+kind create cluster --config=config.yml
+kubectl cluster-info --context kind-kind
+kubectl get nodes
+kind get clusters
