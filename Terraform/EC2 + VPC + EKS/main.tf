01121f099dbe4ffb75ef56c565c23508f078b057
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2025-04-05 01:14:49 +0530

    Updated files = Terraform/EC2 + VPC + EKS/modules/ec2/main.tf Terraform/EC2 + VPC + EKS/modules/ec2/outputs.tf Terraform/EC2 + VPC + EKS/modules/ec2/variables.tf

---
+++ Terraform/EC2 + VPC + EKS/main.tf
@@ -0,0 +1,35 @@
+# Deploy EC2 Instance
+module "ec2_instance" {
+  source            = "./modules/ec2"
+  ec2_name          = "Terraform_instance"
+  ami_id            = "ami-0e35ddab05955cf57"
+  sg_name           = "default_sg_ec2"
+  ec2_instance_name = "t2.micro"
+  ec2_pem_file      = "DevOps"
+  ec2_az            = "ap-south-1b"
+  enable_port_80    = 80
+
+}
+
+# # Deploy VPC
+module "aws_vpc" {
+  source = "./modules/vpc"
+
+}
+
+# Deploy EKS Cluster and Node Group
+module "eks_cluster" {
+  source                = "./modules/eks"
+  eksclustername        = "test_eks_cluster"
+  # subnet1               = "subnet-0eb9abe0b59e8a725"
+  subnet1               = module.aws_vpc.subnet1_id
+  subnet2               = module.aws_vpc.subnet2_id
+  nodegroupname         = "test_eks_node_group"
+  numberofNodes         = 2
+  NodeGroupInstanceType = "t2.micro"
+  numberofmaxNodes      = 3
+  numberofminNodes      = 1
+  max_unavailable       = 2
+
+}
+
---
+++ Terraform/EC2 + VPC + EKS/modules/eks/variables.tf
@@ -0,0 +1,43 @@
+variable "eksclustername" {
+  type    = string
+  default = "my-eks-cluster"
+}
+
+variable "subnet1" {
+  description = "Default Subnet 1"
+  type        = string
+  default     = ""
+}
+
+variable "subnet2" {
+  description = "Default Subnet 2"
+  type        = string
+  default     = ""
+}
+#
+# Node Group variabels
+#
+variable "nodegroupname" {
+  type    = string
+  default = "my-eks-node-group"
+}
+
+variable "numberofNodes" {
+  type    = number
+  default = 2
+}
+
+variable "numberofmaxNodes" {
+  type    = number
+  default = 3
+}
+
+variable "numberofminNodes" {
+  type    = number
+  default = 1
+}
+
+variable "NodeGroupInstanceType" {
+  type    = string
+  default = "t2.micro"
+}
