890dc12e619e5013e6d4cb95c9927b5afd8897a7
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2025-04-05 01:31:36 +0530

    Updated files = Terraform/EC2 + VPC + EKS/backend.tf Terraform/EC2 + VPC + EKS/main.tf Terraform/EC2 + VPC + EKS/modules/eks/data.tf Terraform/EC2 + VPC + EKS/modules/eks/main.tf Terraform/EC2 + VPC + EKS/modules/eks/output.tf Terraform/EC2 + VPC + EKS/modules/eks/provider.tf Terraform/EC2 + VPC + EKS/modules/eks/variables.tf Terraform/EC2 + VPC + EKS/modules/vpc/main.tf Terraform/EC2 + VPC + EKS/modules/vpc/output.tf Terraform/EC2 + VPC + EKS/modules/vpc/varibales.tf

--- Terraform/EC2 + VPC + EKS/modules/vpc/varibales.tf
+++ Terraform/EC2 + VPC + EKS/modules/vpc/varibales.tf
@@ -118,11 +118,11 @@ variable "map_public_ip_on_launch" {
 }
 variable "database_subnets_cidr_1" {
   description = "mention the CIDR block for database subnet"
-  type = string
-  default = "192.168.5.0/24"
+  type        = string
+  default     = "192.168.5.0/24"
 }
 variable "database_subnets_cidr_2" {
   description = "mention the CIDR block for database subnet"
-  type = string
-  default = "192.168.6.0/24"
+  type        = string
+  default     = "192.168.6.0/24"
 }
