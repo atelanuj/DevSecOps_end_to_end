246e5edc0234c0bf068727802be72894a57020d3
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2025-04-05 15:35:44 +0530

    Updated files = Terraform/EC2 + VPC + EKS/main.tf Terraform/EC2 + VPC + EKS/modules/eks/main.tf Terraform/EC2 + VPC + EKS/modules/eks/variables.tf

--- Terraform/EC2 + VPC + EKS/main.tf
+++ Terraform/EC2 + VPC + EKS/main.tf
@@ -1,38 +1,40 @@
 # Deploy EC2 Instance
-module "ec2_instance" {
-  source            = "./modules/ec2"
-  ec2_name          = "Terraform_instance"
-  ami_id            = "ami-0e35ddab05955cf57"
-  sg_name           = "default_sg_ec2"
-  ec2_instance_name = "t2.micro"
-  ec2_pem_file      = "DevOps"
-  ec2_az            = "ap-south-1b"
-  enable_port_80    = 80
+# module "ec2_instance" {
+#   source            = "./modules/ec2"
+  
+  
+#   ec2_name          = "Terraform_instance"
+#   ami_id            = "ami-0e35ddab05955cf57"
+#   sg_name           = "default_sg_ec2"
+#   ec2_instance_name = "t2.micro"
+#   ec2_pem_file      = "DevOps"
+#   ec2_az            = "ap-south-1b"
+#   enable_port_80    = 80
 
-}
+# }
 
 # # Deploy VPC
 module "aws_vpc" {
   source = "./modules/vpc"
 
+  vpc_name = "test_vpc"
+  enable_dhcp_options = true
+  enable_ipv6 = true
 }
 
 # Deploy EKS Cluster and Node Group
-module "eks_cluster" {
-  source = "./modules/eks"
+# module "eks_cluster" {
+#   source = "./modules/eks"
 
-
-  eksclustername = "test_eks_cluster"
-  vpc_id                = module.aws_vpc.vpc_id
-  subnet1               = module.aws_vpc.subnet1_id
-  subnet2               = module.aws_vpc.subnet2_id
-  nodegroupname         = "test_eks_node_group"
-  numberofNodes         = 2
-  NodeGroupInstanceType = "t2.micro"
-  numberofmaxNodes      = 3
-  numberofminNodes      = 1
-  max_unavailable       = 2
-
-
-}
+#   eksclustername        = "test_eks_cluster"
+#   vpc_id                = module.aws_vpc.vpc_id
+#   subnet1               = module.aws_vpc.public_subnet_1
+#   subnet2               = module.aws_vpc.public_subnet_2
+#   nodegroupname         = "test_eks_node_group"
+#   numberofNodes         = 2
+#   NodeGroupInstanceType = "t2.micro"
+#   numberofmaxNodes      = 3
+#   numberofminNodes      = 1
+#   max_unavailable       = 2
+# }
 
