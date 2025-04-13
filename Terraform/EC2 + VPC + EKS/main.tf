6b2b25bdc319f0578e892b6aa639b492bae3411f
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2025-04-14 01:36:31 +0530

    Updated files = Terraform/EC2 + VPC + EKS/main.tf Terraform/EC2 + VPC + EKS/modules/eks/main.tf

--- Terraform/EC2 + VPC + EKS/main.tf
+++ Terraform/EC2 + VPC + EKS/main.tf
@@ -1,18 +1,18 @@
 # # Deploy VPC
-module "aws_vpc" {
-  source = "./modules/vpc"
+# module "aws_vpc" {
+#   source = "./modules/vpc"
 
-  vpc_name    = "test_vpc"
-  enable_ipv6 = true
-  create_igw  = true
-  # public_subnet      = 2
-  enable_NAT_gateway = false
-  # number of public subnets?
-  # number if private subnets?
-  # public subnets required?
-  # private subnets required?
-  # public and database subnet CIDR range?
-}
+#   vpc_name    = "test_vpc"
+#   enable_ipv6 = true
+#   create_igw  = true
+#   # public_subnet      = 2
+#   enable_NAT_gateway = false
+#   # number of public subnets?
+#   # number if private subnets?
+#   # public subnets required?
+#   # private subnets required?
+#   # public and database subnet CIDR range?
+# }
 
 # Deploy EC2 Instance
 # module "ec2_instance" {
@@ -32,19 +32,34 @@ module "aws_vpc" {
 
 # }
 
-# Deploy EKS Cluster and Node Group
-module "eks_cluster" {
-  source = "./modules/eks"
+# # Deploy EKS Cluster and Node Group
+# module "eks_cluster" {
+#   source = "./modules/eks"
 
-  eksclustername        = "test_eks_cluster"
-  vpc_id                = module.aws_vpc.vpc_id
-  subnet1               = module.aws_vpc.public_subnet_1
-  subnet2               = module.aws_vpc.public_subnet_2
-  nodegroupname         = "test_eks_node_group"
-  numberofNodes         = 2
-  NodeGroupInstanceType = "t2.micro"
-  numberofmaxNodes      = 3
-  numberofminNodes      = 1
-  max_unavailable       = 2
+#   eksclustername        = "test_eks_cluster"
+#   vpc_id                = module.aws_vpc.vpc_id
+#   subnet1               = module.aws_vpc.public_subnet_1
+#   subnet2               = module.aws_vpc.public_subnet_2
+#   nodegroupname         = "test_eks_node_group"
+#   numberofNodes         = 2
+#   NodeGroupInstanceType = "t2.micro"
+#   numberofmaxNodes      = 3
+#   numberofminNodes      = 1
+#   max_unavailable       = 2
+# }
+
+
+# TEST
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.8.0"
+  
+  
+    name = "anuj-my-instance"
+    ami  = "ami-0e35ddab05955cf57" # fetch with data source
+    instance_type = "t2.micro"
+    key_name      = "DevOps"
+    availability_zone = data.aws_availability_zones.available_1.names[0]
+    associate_public_ip_address = true
+    
 }
-
