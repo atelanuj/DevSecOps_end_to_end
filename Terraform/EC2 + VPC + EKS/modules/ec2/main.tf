243dbe45245c94b68363a1388dc08ca6871d6962
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2026-04-07 23:44:23 +0530

    Updated files = .github/workflows/terraform.yml

--- Terraform/EC2 + VPC + EKS/modules/ec2/main.tf
+++ Terraform/EC2 + VPC + EKS/modules/ec2/main.tf
@@ -40,20 +40,20 @@
 #   }
 # }
 
-resource "aws_instance" "test-ec2" {
-  ami                         = var.ami_id
-  instance_type               = var.ec2_instance_name
-  associate_public_ip_address = true
-  key_name                    = var.ec2_pem_file
-  # vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  vpc_security_group_ids      = [var.vpc_sg_id]
-  availability_zone = var.ec2_az
-  subnet_id       = var.subnet_id
-  tags = {
-    Name = var.ec2_name
-  }
-  # count = 3
-}
+# resource "aws_instance" "test-ec2" {
+#   ami                         = var.ami_id
+#   instance_type               = var.ec2_instance_name
+#   associate_public_ip_address = true
+#   key_name                    = var.ec2_pem_file
+#   # vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+#   vpc_security_group_ids      = [var.vpc_sg_id]
+#   availability_zone = var.ec2_az
+#   subnet_id       = var.subnet_id
+#   tags = {
+#     Name = var.ec2_name
+#   }
+#   # count = 3
+# }
 
 # # Attach the EBS volume to the EC2 instance
 # resource "aws_volume_attachment" "ec2_volume_attachment" {
@@ -63,3 +63,15 @@ resource "aws_instance" "test-ec2" {
 #   force_detach = true
 #   depends_on   = [aws_ebs_volume.ec2_ebs_volume, aws_instance.test-ec2]
 # }
+
+resource "aws_instance" "example" {
+  ami           = "ami-05d2d839d4f73aafb"  # Amazon Linux 2 (ap-south-1)
+  instance_type = "m7i-flex.large"
+
+  key_name = "anujpem"  # replace with your key
+
+  tags = {
+    Name = "My-EC2-Instance"
+    Environment = "Dev"
+  }
+}
