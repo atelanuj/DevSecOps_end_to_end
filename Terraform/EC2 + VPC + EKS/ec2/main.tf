f231b15d3993ad584ecf9a7b62b31bdc550ce77a
Anuj Atel <122249665+atelanuj@users.noreply.github.com>
2026-04-10 14:35:33 +0530

    Updated files = Terraform/EC2 + VPC + EKS/ec2/provider.tf

--- Terraform/EC2 + VPC + EKS/ec2/main.tf
+++ Terraform/EC2 + VPC + EKS/ec2/main.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "vault-ec2" {
   ami           = "ami-05d2d839d4f73aafb"
   instance_type = "m7i-flex.large"
   key_name      = "anujpem"
   associate_public_ip_address = true
-  security_groups = ["sg-046d026f055e384e9"]
+  vpc_security_group_ids = ["sg-046d026f055e384e9"]
 
   tags = {
     Name = "Vault-Terraform-EC2-by-assumed-role"
